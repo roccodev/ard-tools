136cf6b4d56999c387d3f7ae789107181be811fa
RoccoDev <hey@rocco.dev>
2024-04-13 04:09:19 +0200

    fuse: add argument parser

--- fuse-ard/src/main.rs
+++ fuse-ard/src/main.rs
@@ -1,18 +1,40 @@
-use std::{env, fs::File};
+use std::fs::File;
 
+use clap::{arg, Command};
+use env_logger::Env;
 use fs::ArhFuseSystem;
 use fuser::MountOption;
 
 mod fs;
 
 fn main() {
-    env_logger::init();
-    let arh_path = env::args().nth(1).unwrap();
-    let ard_path = env::args().nth(2).unwrap();
-    let mount_point = env::args().nth(3).unwrap();
-    let arh = File::open(arh_path).unwrap();
-    let ard = File::open(ard_path).unwrap();
+    let cmd = Command::new("fuse-ard")
+        .arg(arg!([mount_point] "where to mount the archive, e.g. /mnt/ard").required(true))
+        .arg(arg!(--arh <FILE> "path to the .arh file").required(true))
+        .arg(arg!(--ard <FILE> "path to the .ard file").required(true))
+        .arg(arg!(-r --readonly "mount the archive as read-only"))
+        .arg(arg!(-d --debug "enable FUSE debugging and debug logs"));
+    let matches = cmd.get_matches();
+
+    let debug = matches.get_flag("debug");
+    env_logger::Builder::from_env(Env::default().default_filter_or(if debug {
+        "debug"
+    } else {
+        "warn"
+    }))
+    .init();
+
+    let arh = File::open(matches.get_one::<String>("arh").unwrap()).unwrap();
+    let ard = File::open(matches.get_one::<String>("ard").unwrap()).unwrap();
     let fs = ArhFuseSystem::load(arh, ard).unwrap();
-    let opts = vec![MountOption::RO, MountOption::CUSTOM("debug".to_string())];
+
+    let mount_point = matches.get_one::<String>("mount_point").unwrap();
+    let mut opts = vec![
+        MountOption::RO,
+        MountOption::CUSTOM("kernel_cache".to_string()),
+    ];
+    if debug {
+        opts.push(MountOption::CUSTOM("debug".to_string()));
+    }
     fuser::mount2(fs, mount_point, &opts).unwrap();
 }
