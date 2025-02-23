3e14c173ef93b7608cc213d032dc7f0f4a2caffc
RoccoDev <hey@rocco.dev>
2025-02-23 03:27:02 +0100

    cli: improved extraction

--- ard-tools/src/extract.rs
+++ ard-tools/src/extract.rs
@@ -1,17 +1,18 @@
 use std::{
-    collections::VecDeque,
     fs::File,
-    io::{BufReader, Cursor, Read, Seek},
-    os::unix::fs::MetadataExt,
+    io::{BufReader, Cursor, Seek},
     path::Path,
     time::Instant,
 };
 
-use anyhow::{anyhow, bail, Context, Result};
-use ardain::{path::ArhPath, ArdReader, DirEntry, FileMeta};
+use anyhow::{bail, Context, Result};
+use ardain::{path::ArhPath, ArdReader, FileMeta};
 use clap::Args;
-use indicatif::ProgressBar;
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use indicatif::{ProgressBar, ProgressStyle};
+use rayon::{
+    current_num_threads, current_thread_index,
+    iter::{IntoParallelIterator, ParallelIterator},
+};
 
 use crate::InputData;
 
@@ -31,23 +32,10 @@ enum ArdAccess<'b> {
 pub fn run(input: &InputData, args: ExtractArgs) -> Result<()> {
     let fs = input.load_fs()?;
     let root_out = Path::new(&args.out_dir);
-    let mut ard_file = input.ard_file()?;
 
     // Extraction steps:
     // 1. Collect path skeleton
     // 2. Extract files
-
-    let start = Instant::now();
-
-    // let mut buf = Vec::new();
-    // buf.try_reserve_exact(ard_file.metadata()?.size().try_into()?)?;
-    // BufReader::with_capacity(ard_file).read_to_end(&mut buf)?;
-    // let buf = std::fs::read(input.in_ard.as_ref().unwrap())?;
-    let elapsed = start.elapsed();
-    println!(
-        "File reading completed in {} seconds.",
-        elapsed.as_secs_f64()
-    );
 
     let mut arh_paths = vec![];
 
@@ -61,32 +49,46 @@ pub fn run(input: &InputData, args: ExtractArgs) -> Result<()> {
         }
     }
 
+    // Sort paths by offset, leads to better access patterns for the underlying ARD file
+    arh_paths.sort_by_cached_key(|path| fs.get_file_info(path).unwrap().offset);
+
     // Extract files
     let start = Instant::now();
-    let progress = ProgressBar::new(arh_paths.len().try_into().unwrap());
-    arh_paths.into_par_iter().try_for_each_init(
-        || Some(input.ard_file().unwrap()),
-        // || (),
-        |ard_file, path| {
-            let Some(file) = fs.get_file_info(&path) else {
-                unreachable!()
-            };
-            let out_path = root_out.join(&path.as_str()[1..]);
-            if let Some(parent) = out_path.parent() {
-                std::fs::create_dir_all(parent)
-                    .with_context(|| format!("failed to create parent dir {parent:?}"))?;
-            }
-            let mut ard_f = ard_file.take().unwrap();
-            ard_f.rewind()?;
-            ArdAccess::File(ard_f.try_clone()?)
-                // ArdAccess::Mem(&buf)
-                .copy_to(&out_path, file)
-                .with_context(|| format!("failed to extract {path}"))?;
-            ard_file.replace(ard_f);
-            progress.inc(1);
-            Ok::<(), anyhow::Error>(())
-        },
-    )?;
+    let progress = ProgressBar::new(arh_paths.len().try_into().unwrap()).with_style(
+        ProgressStyle::with_template(
+            "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} ETA: {eta}",
+        )
+        .unwrap()
+        .progress_chars("##-"),
+    );
+
+    // Open one fd per thread - try_for_each_init seems to call the init function more than once
+    // per thread
+    let thread_fds = (0..current_num_threads())
+        .map(|i| {
+            input
+                .ard_file()
+                .with_context(|| format!("failed to open ARD for thread {i}"))
+        })
+        .collect::<Result<Vec<_>>>()?;
+
+    arh_paths.into_par_iter().try_for_each(|path| {
+        let Some(file) = fs.get_file_info(&path) else {
+            unreachable!()
+        };
+        let out_path = root_out.join(&path.as_str()[1..]);
+        if let Some(parent) = out_path.parent() {
+            std::fs::create_dir_all(parent)
+                .with_context(|| format!("failed to create parent dir {parent:?}"))?;
+        }
+        let mut ard_file = &thread_fds[current_thread_index().unwrap()];
+        ard_file.rewind()?;
+        ArdAccess::File(ard_file.try_clone()?)
+            .copy_to(&out_path, file)
+            .with_context(|| format!("failed to extract {path}"))?;
+        progress.inc(1);
+        Ok::<(), anyhow::Error>(())
+    })?;
     progress.finish();
     let elapsed = start.elapsed();
     println!("Extraction completed in {} seconds.", elapsed.as_secs_f64());
@@ -96,6 +98,8 @@ pub fn run(input: &InputData, args: ExtractArgs) -> Result<()> {
 
 impl<'b> ArdAccess<'b> {
     fn copy_to(&self, out_path: &Path, file: &FileMeta) -> Result<()> {
+        // Here one alternative for uncompressed files could be to use sendfile(2) between the
+        // ard and output fds
         let buf = match self {
             ArdAccess::File(ard) => ArdReader::new(BufReader::new(ard)).entry(file).read(),
             ArdAccess::Mem(ard) => ArdReader::new(Cursor::new(ard)).entry(file).read(),
